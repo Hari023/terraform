5f489e6cb27b449bdbccdaa8131c176b943a7f7f
Hariharan <105987054+Hari023@users.noreply.github.com>
2024-11-14 17:18:23 +0000

    module creation

--- Module/main.tf
+++ Module/main.tf
@@ -1,4 +1,16 @@
+variable "ami" {
+  description = "value for the ami"
+}
+
+variable "instance_type" {
+    description = "value for the instance"
+}
+
 provider "aws" {
-    region = ""
-  
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami = var.ami
+    instance_type = var.instance_type
 }
